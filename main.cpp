4a236b979deb8a5df1d3ff6a2295570bf5ca8e04
Jonathan Easterman <jonathaneasterman@gmail.com>
2015-10-29 19:46:16 -0700

    Cleaned up a lot of the code. Added assignment operators. Changed Makefile to use main.cpp, that should be changed back after we figure out our seg faults"

--- main.cpp
+++ main.cpp
@@ -7,89 +7,72 @@
 #include "UserNetwork.h"
 
 int main()
-{
-  /*
-  DoublyLinkedList<int> dll;
-
-  dll.addToEnd(1);
-  dll.addToEnd(2);
-  dll.addToEnd(3);
-  dll.addToEnd(4);
-  dll.addToEnd(5);
-
-  cout << dll << endl;
-  DoublyLinkedList<int> dll2(dll);
-  cout << dll2 << endl;
-
-  dll.deleteByValue(3);
-  dll.deleteByValue(1);
-  dll.deleteByValue(5);
-
-  cout << dll << endl;
-  cout << dll2 << endl;
-
-  cout << dll.getHead()->data << endl;
-  */
-  
+{ 
   
   WallPost myPost = WallPost("Hi this is my first post", "Jonathan Easterman");
-  //cout << myPost << endl;
+  cout << myPost << endl;
 
   WallPost otherPost = WallPost();
   otherPost.setContent("And this is my second post");
   otherPost.setAuthor("Jonathan Easterman");
   otherPost.setTimePosted();
-  //cout << otherPost.getAuthor() << otherPost.getContent() << otherPost.getTimePosted() << endl;
-  //cout << otherPost << endl;
-
+  cout << otherPost.getAuthor() << otherPost.getContent() << otherPost.getTimePosted() << endl;
+  cout << otherPost << endl;
+/*
   Wall myWall = Wall();
 
   WallPost wallPost = WallPost("Here is my wall!", "WallPost creator");
 
   myWall.addPost(myPost);
+//segfault
+  cout << "make it here?" << endl;
   myWall.addPost(otherPost);
-  //myWall.deletePost(myPost);
+
+  myWall.deletePost(0);
   myWall.addPost(wallPost);
+
+
   
-  //Wall newWall = Wall(myWall);
+
+
+  Wall newWall = Wall(myWall);
   
-  //myWall.deletePost(myPost);
+  myWall.deletePost(0);
   
-  //cout << newWall.toString() << endl;
+  cout << newWall.toString() << endl;
   
  
-  //cout << myWall.toString() << endl;
-  //cout << "\n\n^%^%^&%^&%^&------DESIRED CONTENT ABOVE%$^$%^$%^%$\n\n";
+  cout << myWall.toString() << endl;
+  cout << "\n\n^%^%^&%^&%^&------DESIRED CONTENT ABOVE%$^$%^$%^%$\n\n";
   
-  //cout << myWall.toString() << endl;
+  cout << myWall.toString() << endl;
   
   string myWallStr = myWall.toString();
   Wall myOtherWall = Wall();
   myOtherWall.readWallPostsFromString(myWallStr);
 
-  //cout << myOtherWall.toString() << endl;
+  cout << myOtherWall.toString() << endl;*/
 
   User Ish = User("feshies", "pass123", "Ishi von Meier", "Goleta, CA");
   Ish.addPost(otherPost);
-  User Jon = User("jonny", "pass234", "Jonathan Beasterman", "Queefville, CA");
+  User Jon = User("jonny", "pass234", "Jonathan Beasterman", "Queefville, CA");/*
   Jon.addPost(myPost);
-  /*
   cout << Ish.toString() << endl;
   cout << "\n\n^%^%^&%^&%^&------DESIRED CONTENT ABOVE%$^$%^$%^%$\n\n";
   string ishUserString = Ish.toString();
-  Ish.readUserFromString(ishUserString);
+  //Ish.readUserFromString(ishUserString);
 
-  cout << Ish.toString() << endl;*/
+  cout << Ish.toString() << endl;
 
   
   UserNetwork myNetwork = UserNetwork();
   myNetwork.addUser(Ish);
-  //cout << "trying to add duplicate user..." << endl;
-  //myNetwork.addUser(Ish);
+  cout << "trying to add duplicate user..." << endl;
+  myNetwork.addUser(Ish);
   cout << "trying to add second user..." << endl;
   myNetwork.addUser(Jon);
-  //cout<< "trying to delete first User....." <<endl;
-  //myNetwork.deleteUser(Ish);
+  cout<< "trying to delete first User....." <<endl;
+  myNetwork.deleteUser(Ish);
 
   cout << "trying toString(): ....." << endl;
   string stringToRead = myNetwork.toString();
@@ -104,15 +87,14 @@ int main()
   
   cout << "WE GOT THE FUCKING STRING FROM THE FILE" << endl;
   
-  //cout << "\n\n^%^%^&%^&%^&------DESIRED CONTENT ABOVE%$^$%^$%^%$\n\n";
-  /*
-  UserNetwork copyNetwork = UserNetwork();
+  cout << "\n\n^%^%^&%^&%^&------DESIRED CONTENT ABOVE%$^$%^$%^%$\n\n";
+
   copyNetwork.readUserNetworkFromString(stringToRead);
   cout << "string we read: ...." << endl;
   cout << copyNetwork.toString() << endl;
-  */
+  
 
-  cout << "end of program!" << endl;
+  cout << "end of program!" << endl;*/
 
   return 0;
 }
